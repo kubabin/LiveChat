9bd6c0855336b9fe281c464ecbee5a940f575969
Pavle012 <pavle.tomic.32300@gmail.com>
2026-07-10 12:46:31 +0200

    Sepersted all tabs

---
+++ website/tabs/season.js
@@ -0,0 +1,11 @@
+// ---------- Season tab (disabled example) ----------
+// This tab has no dynamic behavior yet - it's here to show the minimum
+// a new tab needs: an entry in tabs.config.js, a tabs/<id>.html file,
+// and (optionally) this init stub.
+
+window.TAB_MODULES = window.TAB_MODULES || {};
+window.TAB_MODULES.season = {
+  init(panelEl) {
+    // no-op
+  },
+};
